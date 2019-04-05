b576a77134b5ad852d8f2bb4097eb8ac1e3dab16
satadii11 <satadii11@gmail.com>
2019-04-05 14:21:17 +0700

    Undo Last Commit

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "dicodingazuresubmission.database.windows.net";
+    $user = "golok";
+    $pass = "AkuLucuBlog56";
+    $db = "submisi1";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
